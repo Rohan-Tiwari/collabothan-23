8b3be5186a4caa651ab9ea055da1518a3678f49f
Rohan-Tiwari <ronnybt99@gmail.com>
2023-10-18 15:38:55 +0200

    reward points

---
+++ finbest-app/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Email API is running.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+     user: 'ronnybt99@gmail.com',
+     pass: 'jlfp zhbw ubjb bfys',
+    },
+});
+
+app.post('/send-email', (req, res) => {
+    const { to, subject, message } = req.body;
+  
+    const mailOptions = {
+      from: 'ronnybt1999@gmail.com',
+      to,
+      subject,
+      text: message,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Email sending failed:', error);
+        res.status(500).json({ message: 'Email sending failed' });
+      } else {
+        console.log('Email sent:', info.response);
+        res.json({ message: 'Email sent successfully' });
+      }
+    });
+  });
+  
